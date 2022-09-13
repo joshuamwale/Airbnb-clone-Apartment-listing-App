96b578e27e0fde3fc4b38ad444ff06709a809443
Joshua <mwalejoshua89@gmail.com>
2022-09-13 16:57:29 +0300

    wrote function App code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,3 +3,20 @@ import Home from "../home/Home";
 import ApartmentList from "../apartmentlist/ApartmentList";
 import AddApartment from "../apartmentform/AddApartment";
 import { Routes, Route } from "react-router-dom";
+
+
+
+
+function App() {
+  return(
+    <div>
+      <NavBar/> 
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/apartmentlist" element={<ApartmentList />}/>
+          <Route exact path="/apartmentform" element={<AddApartment/>}/>
+      </Routes>
+    </div>
+    );
+}
+ export default App;
