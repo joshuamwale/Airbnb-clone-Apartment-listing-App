e2e683400b0b40bdba18d973f48b8cb152f0c488
Joshua <mwalejoshua89@gmail.com>
2022-09-13 16:17:27 +0300

    modified my NavBar component by adding links

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,3 +4,12 @@ import { Link } from "react-router-dom";
 function NavBar() {
 
 return(
+
+<nav className="navBar">
+      <Link className="active" to="/">Home</Link>
+      <Link to="/apartmentlist">ApartmentList</Link>
+      <Link to="/apartmentform">AddApartment</Link>
+</nav>
+);
+}
+export default NavBar;
