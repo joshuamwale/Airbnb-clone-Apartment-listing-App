5ce7ebf0d0e2a14b7a52a7a83525b6c0d6d830c0
Joshua <mwalejoshua89@gmail.com>
2022-09-13 16:19:15 +0300

    wrote code for my Home component

---
+++ src/components/home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+    return(
+        <div id="home">
+            <center style={{marginTop: 5 +'em', fontSize: 20 + 'px'}}>
+                <p>
+                This is a premier site that lists exquisite apartments you can book and lodge in while on tour or vacation.<br></br>
+                The plan is simple: scroll through the list to find your preffered facility. <br></br>
+                The app also allows you to add other apartments you have previously enjoyed staying in.
+                </p>
+            </center>
+        </div>
+    )
+}
+
+export default Home;
