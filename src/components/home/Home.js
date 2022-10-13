6cc9373bc06c16e010945fa070d479344072ee5e
Joshua <mwalejoshua89@gmail.com>
2022-10-13 08:15:00 +0300

    modified the index.css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,6 +12,7 @@ function Home() {
                 The plan is simple: scroll through the list to find your preffered facility. <br></br>
                 The app also allows you to add other apartments you have previously enjoyed staying in.
                 </p>
+                
                 <Link to="/apartmentlist" className="homebtn">Our Apartments</Link>
             </center>
         </div>
